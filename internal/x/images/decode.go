5aeb2333762fa4b9d765cedc8db7b5908e958f73
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-08-14 16:25:46 +0200

    fix: the decode image normalizes it

--- internal/x/images/decode.go
+++ internal/x/images/decode.go
@@ -10,6 +10,63 @@ import (
 	"gorgonia.org/tensor"
 )
 
+// ImageToBWHC convert an image to a BWHC tensor
+// this function returns an error if:
+//
+//   - dst is not a pointer
+//   - dst's shape is not 4
+//   - dst' second dimension is not 1
+//   - dst's third dimension != i.Bounds().Dy()
+//   - dst's fourth dimension != i.Bounds().Dx()
+//   - dst's type is not float32 or float64 (temporarly)
+func ImageToBWHC(img image.Image, dst tensor.Tensor) error {
+	// check if tensor is a pointer
+	rv := reflect.ValueOf(dst)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("cannot decode image into a non pointer or a nil receiver")
+	}
+	// check if tensor is compatible with BWHC (4 dimensions)
+	if len(dst.Shape()) != 4 {
+		return fmt.Errorf("Expected a 4 dimension tensor, but receiver has only %v", len(dst.Shape()))
+	}
+	// Check the batch size
+	if dst.Shape()[0] != 1 {
+		return errors.New("only batch size of one is supported")
+	}
+	w := img.Bounds().Dx()
+	h := img.Bounds().Dy()
+	if dst.Shape()[1] != h || dst.Shape()[2] != w {
+		return fmt.Errorf("cannot fit image into tensor; image is %v*%v but tensor is %v*%v", h, w, dst.Shape()[2], dst.Shape()[3])
+	}
+	switch dst.Dtype() {
+	case tensor.Float32:
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				r, g, b, a := img.At(x, y).RGBA()
+				if a != 65535 {
+					return errors.New("transparency not handled")
+				}
+				err := dst.SetAt(float32(uint8(r/0x101))/255, 0, x, y, 1)
+				if err != nil {
+					return err
+				}
+				err = dst.SetAt(float32(uint8(g/0x101))/255, 0, x, y, 1)
+				if err != nil {
+					return err
+				}
+				err = dst.SetAt(float32(uint8(b/0x101))/255, 0, x, y, 2)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	default:
+		return fmt.Errorf("%v not handled yet", dst.Dtype())
+	}
+	return nil
+
+}
+
 // ImageToBCHW convert an image to a BCHW tensor
 // this function returns an error if:
 //
